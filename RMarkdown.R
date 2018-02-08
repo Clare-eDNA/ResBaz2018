a87cbab0e79435310911b57dc02678276cf27714
Turdlez <ciadams@iastate.edu>
2018-02-09 12:05:15 +1300

    ugh whatever

--- RMarkdown.R
+++ RMarkdown.R
@@ -35,3 +35,37 @@
 
 #Making lists / nested lists
 #make sure you have two tabs
+
+#R Markdown Tables
+# Syntax for basic table
+table 1
+
+col 1 | col 2 | col 3
+--- | --- | ---
+  row1 | 1 | 2
+row 2 | 3 | 4
+
+
+#Ctrl+enter runs code so you can get an idea of what is going to happen
+#alignment can be r c or l (right, center or left) (you can do one letter for each column)
+
+#Markdown tables, kabletables... digits = round to two digits, rename columns, align to the left or right (or center), add in captions
+kable(data.frame(col1=rnorm(20), col2=rnorm(20)), digits =2, col.names = c("Column 1", "Column 2"), align = "lr", caption = "My caption")
+library(kableExtra)
+
+#How to make sure the table has the same number of decimal points
+data.frame(col1=rnorm(20), col2=rnorm(20)) %>%
+  mutate(col1=sprintf('%.3f',col1)) %>% #sprint f will return a character, the % means substitue in the first argument coming in (col1), the dot indicates the decimal, then we want three figures after the decimal, the f indicates the object coming in is a decimal number. So this function will take everything to three zeros, then pipe it through the end of the table with %>%
+  #Make sure to do this as the last thing before it hits the formatting table
+  kable(., digits=2, col.names=c("Column 1",  "Column 2"), align = "lr", caption = "My caption")
+
+#You can make kableTables to have headers that cover multiple columns
+kable(., 
+      digits=2, 
+      col.names=c("Column 1",  "Column 2"),
+      align = "lr",
+      caption = "My caption", 
+      format="html") %>% 
+  group_rows("first5",1,5) %>%
+  group_rows('second5',6,10)%>%
+  add_header_above(c("Group 1" = 2)) 
