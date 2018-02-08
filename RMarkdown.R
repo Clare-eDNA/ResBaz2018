a4b4ccdae7744d0c42e79393148e74ede0df3cf9
Turdlez <ciadams@iastate.edu>
2018-02-09 10:03:49 +1300

    updated notes

--- RMarkdown.R
+++ RMarkdown.R
@@ -1,1 +1,7 @@
 # R Markdown
+# Reproducable Research, best practice methods
+
+#Step 1: make a new project
+  #New directory -> new project -> chuck it somewhere useful
+  #If you check "create a git repo, 
+  #Generally make folders (Data, Source, Results, Docs)
