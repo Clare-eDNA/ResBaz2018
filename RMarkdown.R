ade40a1a2151a1c20ba1534a9e21801dd29b010b
Turdlez <ciadams@iastate.edu>
2018-02-09 10:23:33 +1300

    Not really worth saving the html files, added Rmd file, my first knit

--- RMarkdown.R
+++ RMarkdown.R
@@ -5,3 +5,23 @@
   #New directory -> new project -> chuck it somewhere useful
   #If you check "create a git repo, 
   #Generally make folders (Data, Source, Results, Docs)
+  #If you don't want your data online -> .gitignore -> type data/*
+
+#R Markdown
+#File > New File > R Markdown... > Follow prompts
+#You get a new doc and then get code + text stuff
+#instead of html_document you can change to pdf_document but it requires LaTeX
+#You can always use Help > Markdown Quick Reference
+
+#Code chunks can be executable by R in an R markdown file rather than just a Markdown
+
+#To output, click on the knitting needles/Knitr, and then save the file to wherever you want
+#You'll get another document that'll show you how progress goes in the console (bottom left)
+#Your file should come up automatically once the file is created
+
+#To make a code chunk... ctrl+alt+i
+# include=FALSE is when you don't want the code chunk to show up
+# echo = TRUE or FALSE - tells whether the command piece of the code should be output
+  #But still spits out the result
+# eval = TRUE or FALSE - shows the command but not the output
+  #Really good for isolating problems
